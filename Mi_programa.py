2725ac289a7251fd22fcdececbc721c916ad3d6e
pepeantuan <jnavarrosa@gmail.com>
2026-02-19 20:14:42 +0100

    Create Mi_programa.py

---
+++ Mi_programa.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+
+# 1. Mostrar mensaje
+print("Hola Mundo Cruel!!!")
+
+# 2. Mostrar día y hora actual
+ahora = datetime.now()
+print("Fecha y hora actual:", ahora.strftime("%d/%m/%Y %H:%M:%S"))
+
+ 
